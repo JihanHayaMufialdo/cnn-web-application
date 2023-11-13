1c330af1ed27e1a6f1698562492d78486cea5252
kamal <ramadhan131103senin@gmail.com>
2023-11-13 13:58:10 +0700

    membuat navbar dan sidebar

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dosen', function(){
+    return view('web.dashboards.dashboard');
+})->name('dosen-dashboard');
